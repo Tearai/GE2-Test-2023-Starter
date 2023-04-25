0b73b8cfcaa815540829f7aeddf4236b237d7cd0
Tearai <77899947+Tearai@users.noreply.github.com>
2023-04-25 09:47:40 +0100

    Update 1

---
+++ Unity/Assets/RandomColor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+ 
+
+
+    IEnumerator WaitAndPrint()
+    {
+        // suspend execution for 5 seconds
+        
+        ChangeColor();
+        yield return new WaitForSeconds(1);
+    }
+
+    public void Start()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+
+
+    public void Update()
+    {
+        StartCoroutine(WaitAndPrint());
+        
+    }
+
+    public void ChangeColor()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+}
